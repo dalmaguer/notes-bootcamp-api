ff05e37351c0860f2a3fa4a6f665238c18ce9978
Dayrel Almaguer <dayrel.almaguer@gmail.com>
2021-03-16 14:51:40 -0400

    basic notes api endpoints (get, delete, create)

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const http = require('http')
+const getNextId = require('./utils/index')
+const express = require('express')
 
-const notes = [{
+const app = express()
+app.use(express.json())
+
+let notes = [{
   id: 1,
   content: 'HTML is easy',
   date: '2019-05-30T17:30:31.098Z',
@@ -10,13 +14,66 @@ const notes = [{
   content: 'Browser can execute only javascript',
   date: '2019-05-30T17:39:34.091Z',
   important: false
+}, {
+  id: 3,
+  content: 'React is Awesome',
+  date: '2019-05-30T17:39:34.091Z',
+  important: true
 }]
 
-const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain'})
-    response.end('Hello World!')
+// ---------- With Node --------------------
+// const http = require('http')
+
+// const app = http.createServer((request, response) => {
+//     response.writeHead(200, { 'Content-Type': 'application/json'})
+//     response.end(JSON.stringify(notes))
+// })
+
+// ----------- With Express ----------------
+
+app.get('/',(request, response) => {
+  response.send('<h1>My Notes API 🚀</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const {id} = request.params
+  const note = notes.find(n => n.id === Number(id))
+
+  note
+  ? response.json(note)
+  : response.status(404).end()
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const {id} = request.params
+  notes = notes.filter(item => item.id !== Number(id))
+  response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+  const note = request.body
+
+  if(!note || !note.content){
+    return response.status(400).json({
+      error: 'note.content is missing'
+    })
+  }
+
+  const newNote = {
+    id: getNextId(notes),
+    content: note.content,
+    date: new Date().toISOString(),
+    important: note.important || false
+  }
+  notes = [...notes, newNote]
+  response.status(201).json(newNote)
 })
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
---
+++ utils/index.js
@@ -0,0 +1,15 @@
+const getWithMaxIndex = (arr) => {
+  if (Array.isArray(arr) && arr.length) {
+    return arr.reduce((acc, current) => {
+      return Number(current.id) > Number(acc.id) ? current : acc
+    })
+  }
+  return 0
+}
+
+const getNextId = (arr) => {
+  const last = getWithMaxIndex(arr)
+  return Number(last.id) + 1
+}
+
+module.exports = getNextId
